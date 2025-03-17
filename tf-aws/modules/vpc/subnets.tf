e8b88221572cb656640672e0c11e7b57b92c62a3
gavwyh <121929957+gavwyh@users.noreply.github.com>
2025-03-17 15:48:11 +0800

    refactor: refactor vpc module

--- tf-aws/modules/vpc/subnets.tf
+++ tf-aws/modules/vpc/subnets.tf
@@ -27,7 +27,7 @@ resource "aws_subnet" "private_subnet_2" {
 }
 
 resource "aws_db_subnet_group" "db_subnet_group" {
-  name = "brawlcode-db-subnet-group"
+  name = "scrooge-bank-db-subnet-group"
   subnet_ids = [
     aws_subnet.private_subnet_1.id,
     aws_subnet.private_subnet_2.id
