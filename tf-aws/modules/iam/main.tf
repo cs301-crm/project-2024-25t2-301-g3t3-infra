47e457f1c174384c2aef415d3ce0f62d59f9acdb
gavwyh <121929957+gavwyh@users.noreply.github.com>
2025-03-17 16:39:10 +0800

    feat: add ec2 iam roles for eks

--- tf-aws/modules/iam/main.tf
+++ tf-aws/modules/iam/main.tf
@@ -20,3 +20,36 @@ resource "aws_iam_role_policy_attachment" "prod-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role = aws_iam_role.eks.name
 }
+
+resource "aws_iam_role" "nodes" {
+  name = "eks-node-group-nodes"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEKSWorkerNodePolicy" {
+  # Grant access to EC2 and EKS
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role = aws_iam_role.nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role = aws_iam_role.nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role = aws_iam_role.nodes.name
+}
--- tf-aws/modules/iam/outputs.tf
+++ tf-aws/modules/iam/outputs.tf
@@ -7,3 +7,17 @@ output "eks_cluster_role_policy_attachment" {
   description = "Role policy attachment of EKS cluster role"
   value = aws_iam_role_policy_attachment.prod-AmazonEKSClusterPolicy
 }
+
+output "eks_node_role_arn" {
+  description = "ARN of EKS node group"
+  value = aws_iam_role.nodes.arn
+}
+
+output "eks_node_role_policy_attachments" {
+  description = "Role policy attachment of EKS node role"
+  value = [
+    aws_iam_role_policy_attachment.nodes-AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.nodes-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.nodes-AmazonEKSWorkerNodePolicy
+  ]
+}
