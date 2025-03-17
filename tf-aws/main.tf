77ef209623ec9428ac4173aae17c21d37a8fb1e4
gavwyh <121929957+gavwyh@users.noreply.github.com>
2025-03-17 15:47:29 +0800

    refactor: refactor vpc module

--- tf-aws/main.tf
+++ tf-aws/main.tf
@@ -1,9 +1,6 @@
-variable "az" {}
-
 module "vpc" {
   source   = "./modules/vpc"
   vpc_cidr = "10.0.0.0/16"
-  az       = var.az
 }
 
 module "dynamodb" {
---
+++ tf-aws/modules/vpc/subnets.tf
@@ -0,0 +1,35 @@
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.vpc.cidr_block, 8, 1) # 10.0.1.0
+  availability_zone       = "ap-southeast-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.vpc.cidr_block, 8, 2)
+  availability_zone       = "ap-southeast-1b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.vpc.cidr_block, 8, 3)
+  availability_zone       = "ap-southeast-1a"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.vpc.cidr_block, 8, 4)
+  availability_zone       = "ap-southeast-1b"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_db_subnet_group" "db_subnet_group" {
+  name = "brawlcode-db-subnet-group"
+  subnet_ids = [
+    aws_subnet.private_subnet_1.id,
+    aws_subnet.private_subnet_2.id
+  ]
+}
