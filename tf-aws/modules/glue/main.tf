7d87402f741a9a1c827c3065d1f557f2d78385e3
gavwyh <121929957+gavwyh@users.noreply.github.com>
2025-03-22 13:23:00 +0800

    feat: add notif, otp schema

--- tf-aws/modules/glue/main.tf
+++ tf-aws/modules/glue/main.tf
@@ -9,3 +9,19 @@ resource "aws_glue_schema" "log_schema" {
   compatibility     = "BACKWARD"
   schema_definition = var.log_schema_definition
 }
+
+resource "aws_glue_schema" "otp_schema" {
+  schema_name       = "log_schema"
+  registry_arn      = aws_glue_registry.scrooge-bank-registry.arn
+  data_format       = var.data_format
+  compatibility     = "BACKWARD"
+  schema_definition = var.otp_schema_definition
+}
+
+resource "aws_glue_schema" "notification_schema" {
+  schema_name       = "log_schema"
+  registry_arn      = aws_glue_registry.scrooge-bank-registry.arn
+  data_format       = var.data_format
+  compatibility     = "BACKWARD"
+  schema_definition = var.notification_schema_definition
+}
--- tf-aws/modules/glue/variables.tf
+++ tf-aws/modules/glue/variables.tf
@@ -7,5 +7,17 @@ variable "data_format" {
 variable "log_schema_definition" {
   description = "Schema of the kafka log message"
   type = string
-  default = "syntax = \"proto3\";\npackage com.cs301.crm;\nmessage Log {\n    string log_id = 1;\n  string actor = 2;\n  string transaction_type = 3;\n   string action = 4;\n   string timestamp = 5;\n}"
+  default = "syntax = \"proto3\";\n\npackage com.cs301.crm;\n\noption java_multiple_files = true;\noption java_package = \"com.cs301.crm.protobuf\";\n\nmessage Log {\n  string log_id = 1;\n  string actor = 2;\n  string transaction_type = 3;\n  string action = 4;\n  string timestamp = 5;\n}"
+}
+
+variable "notification_schema_definition" {
+  description = "Schema of the kafka notification message, which is used to notify newly created users"
+  type = string
+  default = "syntax = \"proto3\";\n\npackage com.cs301.crm;\n\noption java_multiple_files = true;\noption java_package = \"com.cs301.crm.protobuf\";\n\nmessage Notification {\n  string email = 1;\n  string username = 2;\n  string tempPassword = 3;\n  string role = 4;\n}"
+}
+
+variable "otp_schema_definition" {
+  description = "Schema of the kafka otp message"
+  type = string
+  default = "syntax = \"proto3\";\n\npackage com.cs301.crm;\n\noption java_multiple_files = true;\noption java_package = \"com.cs301.crm.protobuf\";\n\nmessage Otp {\n  string email = 1;\n  uint32 otp = 2;\n  string timestamp = 3;\n}"
 }
