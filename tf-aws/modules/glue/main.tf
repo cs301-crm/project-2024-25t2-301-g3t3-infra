9d56d1bc1af47107331c1252aede82163059d0bd
gavwyh <121929957+gavwyh@users.noreply.github.com>
2025-03-22 13:23:19 +0800

    refactor: sync schema name to topic name

--- tf-aws/modules/glue/main.tf
+++ tf-aws/modules/glue/main.tf
@@ -2,24 +2,24 @@ resource "aws_glue_registry" "scrooge-bank-registry" {
   registry_name = "scrooge-bank-registry"
 }
 
-resource "aws_glue_schema" "log_schema" {
-  schema_name       = "log_schema"
+resource "aws_glue_schema" "logs" {
+  schema_name       = "logs"
   registry_arn      = aws_glue_registry.scrooge-bank-registry.arn
   data_format       = var.data_format
   compatibility     = "BACKWARD"
   schema_definition = var.log_schema_definition
 }
 
-resource "aws_glue_schema" "otp_schema" {
-  schema_name       = "log_schema"
+resource "aws_glue_schema" "otps" {
+  schema_name       = "otps"
   registry_arn      = aws_glue_registry.scrooge-bank-registry.arn
   data_format       = var.data_format
   compatibility     = "BACKWARD"
   schema_definition = var.otp_schema_definition
 }
 
-resource "aws_glue_schema" "notification_schema" {
-  schema_name       = "log_schema"
+resource "aws_glue_schema" "notifications" {
+  schema_name       = "notifications"
   registry_arn      = aws_glue_registry.scrooge-bank-registry.arn
   data_format       = var.data_format
   compatibility     = "BACKWARD"
