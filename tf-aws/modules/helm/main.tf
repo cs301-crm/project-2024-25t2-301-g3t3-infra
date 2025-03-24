76dc390b038d3b2086a88b0fc79a6628c78854d5
gavwyh <121929957+gavwyh@users.noreply.github.com>
2025-03-24 13:50:38 +0800

    feat: add argocd and argo imageupdater

---
+++ tf-aws/modules/helm/main.tf
@@ -0,0 +1,49 @@
+resource "helm_release" "aws_lbc" {
+  name = "aws-load-balancer-controller"
+
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  version    = "1.7.2"
+
+  set {
+    name  = "clusterName"
+    value = var.eks_cluster_name
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+  depends_on = [var.eks_private_nodes]
+}
+
+# kubectl get secrets argocd-initial-admin-secret -o yaml -n argocd
+# take password and echo "<password>" | base64 -d
+
+resource "helm_release" "argocd" {
+  name = "argocd"
+
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+  version          = "3.35.4"
+
+  values = [file("${path.module}/values/argocd.yaml")]
+
+  depends_on = [var.eks_private_nodes]
+}
+
+resource "helm_release" "image-updater" {
+  name = "image-updater"
+
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argocd-image-updater"
+  namespace        = "argocd"
+  create_namespace = true
+  version          = "0.12.0"
+
+  values = [file("${path.module}/values/image-updater.yaml")]
+  depends_on = [helm_release.argocd]
+}
---
+++ tf-aws/modules/helm/variables.tf
@@ -0,0 +1,7 @@
+variable "eks_cluster_name" {
+  description = "Name of EKS cluster"
+}
+
+variable "eks_private_nodes" {
+  description = "Private node groups of our EKS"
+}
