87be3b73e6e5ab8247e6d84b41ed3cc2281c7bbd
gavwyh <121929957+gavwyh@users.noreply.github.com>
2025-03-25 18:19:54 +0800

    feat: add cert manager

--- tf-aws/modules/helm/main.tf
+++ tf-aws/modules/helm/main.tf
@@ -92,3 +92,17 @@ resource "helm_release" "nginx_ingress" {
 
   depends_on = [helm_release.aws_lbc]
 }
+
+resource "helm_release" "cert_manager" {
+  name = "cert-manager"
+
+  repository = "https://charts.jetstack.io"
+  chart = "cert-manager"
+  namespace = "cert-manager"
+  create_namespace = true
+  version = "v1.17.1"
+
+  values = [file("${path.module}/values/cert-manager.yaml")]
+
+  depends_on = [helm_release.nginx_ingress]
+}
