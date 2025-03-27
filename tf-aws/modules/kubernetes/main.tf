4ebcb5686fa4100abf9757b8e96c9d9cc8899609
gavwyh <121929957+gavwyh@users.noreply.github.com>
2025-03-27 14:17:35 +0800

    feat: add volume mount to k8s for efs

--- tf-aws/modules/kubernetes/main.tf
+++ tf-aws/modules/kubernetes/main.tf
@@ -86,3 +86,19 @@ resource "kubernetes_cluster_role_binding" "prometheus-k8s" {
     api_group = "rbac.authorization.k8s.io"
   }
 }
+
+resource "kubernetes_storage_class_v1" "efs" {
+  metadata {
+    name = "efs"
+  }
+
+  storage_provisioner = "efs.csi.aws.com"
+
+  parameters = {
+    provisioningMode = "efs-ap"
+    fileSystemId     = var.efs_file_system_id
+    directoryPerms   = "700"
+  }
+
+  mount_options = ["iam"]
+}
