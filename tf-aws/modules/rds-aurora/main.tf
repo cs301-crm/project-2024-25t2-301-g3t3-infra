6ada0f21a4b7dc93332d4724f6b7fa6f23a8c66c
yijiyap <yijiyap@gmail.com>
2025-03-31 10:22:33 +0800

    move var aurora

--- tf-aws/modules/rds-aurora/main.tf
+++ tf-aws/modules/rds-aurora/main.tf
@@ -1,7 +1,3 @@
-variable "database_subnet_ids" {}
-variable "aurora_kms_key_id" {}
-variable "rds_sg_id" {}
-
 resource "aws_rds_cluster" "main" {
   cluster_identifier            = "aurora-cluster"
   engine                        = "aurora-postgresql"
