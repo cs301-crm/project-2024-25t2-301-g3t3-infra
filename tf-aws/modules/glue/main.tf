02e4e5e818e8c9af1462570bf60634065245af50
gavwyh <121929957+gavwyh@users.noreply.github.com>
2025-03-31 20:55:22 +0800

    feat: add all schemas

--- tf-aws/modules/glue/main.tf
+++ tf-aws/modules/glue/main.tf
@@ -2,26 +2,42 @@ resource "aws_glue_registry" "scrooge-bank-registry" {
   registry_name = "scrooge-bank-registry"
 }
 
-resource "aws_glue_schema" "logs" {
-  schema_name       = "logs"
+resource "aws_glue_schema" "log" {
+  schema_name       = "log"
   registry_arn      = aws_glue_registry.scrooge-bank-registry.arn
   data_format       = var.data_format
   compatibility     = "BACKWARD"
   schema_definition = var.log_schema_definition
 }
 
-resource "aws_glue_schema" "otps" {
-  schema_name       = "otps"
+resource "aws_glue_schema" "otp" {
+  schema_name       = "otp"
   registry_arn      = aws_glue_registry.scrooge-bank-registry.arn
   data_format       = var.data_format
   compatibility     = "BACKWARD"
   schema_definition = var.otp_schema_definition
 }
 
-resource "aws_glue_schema" "notifications" {
-  schema_name       = "notifications"
+resource "aws_glue_schema" "u2c" {
+  schema_name       = "u2c"
   registry_arn      = aws_glue_registry.scrooge-bank-registry.arn
   data_format       = var.data_format
   compatibility     = "BACKWARD"
-  schema_definition = var.notification_schema_definition
+  schema_definition = var.u2c_schema_definition
+}
+
+resource "aws_glue_schema" "a2c" {
+  schema_name       = "a2c"
+  registry_arn      = aws_glue_registry.scrooge-bank-registry.arn
+  data_format       = var.data_format
+  compatibility     = "BACKWARD"
+  schema_definition = var.a2c_schema_definition
+}
+
+resource "aws_glue_schema" "c2c" {
+  schema_name       = "c2c"
+  registry_arn      = aws_glue_registry.scrooge-bank-registry.arn
+  data_format       = var.data_format
+  compatibility     = "BACKWARD"
+  schema_definition = var.c2c_schema_definition
 }
