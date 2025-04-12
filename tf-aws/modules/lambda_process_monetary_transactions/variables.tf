91be21eb3e96ea0fbe0666b9b7764a7f6782848e
yijiyap <yijiyap@gmail.com>
2025-04-12 17:27:41 +0800

    new lambda to rerun when prev lambda fails

--- tf-aws/modules/lambda_process_monetary_transactions/variables.tf
+++ tf-aws/modules/lambda_process_monetary_transactions/variables.tf
@@ -4,4 +4,5 @@ variable "private_subnet_ids" {}
 variable "lambda_sg_id" {}
 variable "rds_cluster_secret_arn" {}
 variable "mt_queue_arn" {}
+variable "mt_dlq_queue_arn" {}
 variable "db_proxy_lambdas_endpoint" {}
--- tf-aws/modules/sqs/main.tf
+++ tf-aws/modules/sqs/main.tf
@@ -15,6 +15,7 @@ resource "aws_sqs_queue" "mt_queue" {
 
 resource "aws_sqs_queue" "mt_queue_deadletter" {
   name = "sftp-s3-event-notification-deadletter-queue"
+  policy = data.aws_iam_policy_document.queue.json
 }
 
 resource "aws_sqs_queue_redrive_allow_policy" "mt_redrive_allow_policy" {
