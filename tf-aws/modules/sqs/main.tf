8d1fcacd6826ade8d22ff383e44eb9165620e665
Yiji Yap <yijiyap@gmail.com>
2025-04-13 00:44:17 +0800

    hotfix: add retention period (#35)

--- tf-aws/modules/sqs/main.tf
+++ tf-aws/modules/sqs/main.tf
@@ -16,6 +16,9 @@ resource "aws_sqs_queue" "mt_queue" {
 resource "aws_sqs_queue" "mt_queue_deadletter" {
   name   = "sftp-s3-event-notification-deadletter-queue"
   policy = data.aws_iam_policy_document.queue.json
+
+  message_retention_seconds = 86400
+  visibility_timeout_seconds = 120
 }
 
 resource "aws_sqs_queue_redrive_allow_policy" "mt_redrive_allow_policy" {
